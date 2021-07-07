5c628c793f99f6d00e2658cb44f3f7946feb1212
Aynur Galimov <galimovaynur@protonmail.com>
2021-07-07 18:26:28 +0500

    add serializer

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,7 +1,7 @@
 class Band < ApplicationRecord
   has_many :members
 
-  def as_json(options={})
-    super(except: [:created_at, :updated_at], include: :members)
-  end
+  # def as_json(options={})
+  #   super(except: [:created_at, :updated_at], include: :members)
+  # end
 end
